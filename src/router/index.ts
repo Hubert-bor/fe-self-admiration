329aae565648a3f33ef749cfe15f359b6ac9db03
ztqq <sep10_n@163.com>
2025-02-24 15:30:32 +0800

    feat: ts 配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/views/HomePage.vue'
-import ThreeDWordCloud from '@/components/ThreeDWordCloud.vue'
-
+// import HomePage from '@/views/HomePage.vue'
+import ThreeDWordCloud from '@/views/ThreeDWordCloud.vue'
+console.log('import.meta', import.meta)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomePage
+      redirect: '/wordCloud'
+      // component: HomePage
     },
     {
       path: '/wordCloud',
